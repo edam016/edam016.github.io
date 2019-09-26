5dbcd7a2dedd3e57b53bb9218c1d7cdb1d143457
edam016 <edam3268@gmail.com>
2019-09-26 02:57:02 -0400

    Cleaned up Css

--- site.js
+++ site.js
@@ -1,23 +1,23 @@
 function bgChanger() {
     console.log(this.scrollY);
-    if (this.scrollY > 307 && this.scrollY < 824) {
+    if (this.scrollY > 406 && this.scrollY < 824) {
         document.body.classList.add('bg-change1');
         document.body.classList.remove('bg-change2');
     }
-    if (this.scrollY < 307) {
+    if (this.scrollY < 406) {
         document.body.classList.remove("bg-change1");
     }
-    if (this.scrollY > 824 && this.scrollY < 1850) {
+    if (this.scrollY > 824 && this.scrollY < 2940) {
         document.body.classList.remove('bg-change1');
         document.body.classList.remove('bg-change3');
         document.body.classList.add('bg-change2');
     }
-    if (this.scrollY > 1500 && this.scrollY < 4200) {
+    if (this.scrollY > 2940 && this.scrollY < 4000) {
         document.body.classList.remove('bg-change2');
         document.body.classList.remove('bg-change4');
         document.body.classList.add('bg-change3');
     }
-    if (this.scrollY > 4200 && this.scrollY < 6069) {
+    if (this.scrollY > 4000 && this.scrollY < 6069) {
         document.body.classList.remove('bg-change3');
         document.body.classList.remove('bg-change5');
         document.body.classList.add('bg-change4');
