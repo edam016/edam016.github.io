89c68e1d494507c2a3d71ccba0598eaf2f3f1494
edam016 <edam3268@gmail.com>
2019-09-27 03:10:25 -0400

    Changed background pattern

--- site.js
+++ site.js
@@ -1,30 +1,40 @@
 function bgChanger() {
     console.log(this.scrollY);
-    if (this.scrollY > 406 && this.scrollY < 824) {
+    if (this.scrollY > 406 && this.scrollY < 936) {
         document.body.classList.add('bg-change1');
         document.body.classList.remove('bg-change2');
     }
     if (this.scrollY < 406) {
         document.body.classList.remove("bg-change1");
     }
-    if (this.scrollY > 824 && this.scrollY < 2940) {
+    if (this.scrollY > 936 && this.scrollY < 1500) {
         document.body.classList.remove('bg-change1');
         document.body.classList.remove('bg-change3');
         document.body.classList.add('bg-change2');
     }
-    if (this.scrollY > 2940 && this.scrollY < 4000) {
+    if (this.scrollY > 1500 && this.scrollY < 1760) {
         document.body.classList.remove('bg-change2');
         document.body.classList.remove('bg-change4');
         document.body.classList.add('bg-change3');
     }
-    if (this.scrollY > 4000 && this.scrollY < 6069) {
+    if (this.scrollY > 1760 && this.scrollY < 2700) {
         document.body.classList.remove('bg-change3');
         document.body.classList.remove('bg-change5');
         document.body.classList.add('bg-change4');
     }
-    if (this.scrollY > 6069) {
+    if (this.scrollY > 2700 && this.scrollY < 4000) {
         document.body.classList.remove('bg-change4');
         document.body.classList.add('bg-change5');
+        document.body.classList.remove('bg-change6')
+    }
+    if (this.scrollY > 4000 && this.scrollY < 4800) {
+        document.body.classList.remove('bg-change5');
+        document.body.classList.add('bg-change6');
+        document.body.classList.remove('bg-change7')
+    }
+    if (this.scrollY > 4800 && this.scrollY < 9000) {
+        document.body.classList.remove('bg-change6');
+        document.body.classList.add('bg-change7');
 
     }
 }
