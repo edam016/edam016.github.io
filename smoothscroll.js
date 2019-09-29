256986df914cb6162dfec78bdcb4f0d9b982ef0a
edam016 <edam3268@gmail.com>
2019-09-29 04:01:40 -0400

    Linked buttons

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,36 +1,35 @@
-// Select all links with hashes
-$('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            &&
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                });
-            }
+$(document).ready(function () {
+    $('a').on('click', function (event) {
+
+        var href = $(this).attr('href');
+        if (href == '#about' || href == '#cv' || href == '#mailMe') {
+
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1250, function () {
+
+                window.location.hash = hash;
+            });
         }
     });
+});
+
+$(document).ready(function () {
+
+    $('body').css('display', 'none');
+    $('body').delay(100).fadeIn(1250);
+});
+
+$(document).ready(function () {
+
+    $(this).scrollTop(0);
+});
+
+$('#backToTop').on("click", function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1750)
+});
