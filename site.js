9ab5d9fbacb68f99e339b605e1098be8d47da345
edam016 <edam3268@gmail.com>
2019-09-29 03:28:14 -0400

    Added js for smoothscroll IP

--- site.js
+++ site.js
@@ -39,10 +39,35 @@ function bgChanger() {
     }
 }
 
+window.addEventListener("scroll", bgChanger);
+window.addEventListener('scroll', scrollAppear);
 function smoothScroll(target, duration) {
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect();
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
 
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
+        }
+    }
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+    requestAnimationFrame(animation);
 }
-window.addEventListener("scroll", bgChanger);
-window.addEventListener('scroll', scrollAppear);
+
+var section1 = documentquerySelector('.section1');
+section1.addEventListener('click', function () {
+    smoothScroll('.hi', 1000);
+})
+
