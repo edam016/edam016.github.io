57a40e44d9e60a9030d6d3a5956c230105ec5d2c
edam016 <edam3268@gmail.com>
2019-09-29 16:41:49 -0400

    Changed HC pic

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,35 +1,13 @@
-$(document).ready(function () {
-    $('a').on('click', function (event) {
 
-        var href = $(this).attr('href');
-        if (href == '#about' || href == '#cv' || href == '#mailMe') {
+$('.options a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
 
-            event.preventDefault();
-            var hash = this.hash;
+        const hash = this.hash;
 
-            $('html, body').animate({
+        $('html, body')
+            .animate({
                 scrollTop: $(hash).offset().top
-            }, 1250, function () {
-
-                window.location.hash = hash;
-            });
-        }
-    });
-});
-
-$(document).ready(function () {
-
-    $('body').css('display', 'none');
-    $('body').delay(100).fadeIn(1250);
-});
-
-$(document).ready(function () {
-
-    $(this).scrollTop(0);
-});
-
-$('#backToTop').on("click", function () {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 1750)
+            }, 800);
+    }
 });
